6f6c796bca57e71ca5a826f1394e1a848c6bcc62
Akindu Bandaranayake <avdgroup185@gmail.com>
2024-07-22 23:13:18 +0530

    created the files

--- LaBellaPizza-client/src/router/Router.jsx
+++ LaBellaPizza-client/src/router/Router.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world! ahh akindu</div>,
+      element: <div>Hello world! ahh akindu bandaranayake</div>,
     },
   ]);
 
