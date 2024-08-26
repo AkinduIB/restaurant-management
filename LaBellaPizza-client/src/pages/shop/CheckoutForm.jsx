3695b7a625694944400bfdf90703be175ccddca5
Akindu Bandaranayake <avdgroup185@gmail.com>
2024-08-27 01:50:59 +0530

    completed payment process, manage booking and admin/staff dashboards

--- LaBellaPizza-client/src/pages/shop/CheckoutForm.jsx
+++ LaBellaPizza-client/src/pages/shop/CheckoutForm.jsx
@@ -38,8 +38,8 @@ const CheckoutForm = ({ price, cart }) => {
         <div className='flex flex-col sm:flex-row justify-start items-start gap-8'>
             <div className='md:w-1/2 w-full space-y-3'>
                 <h4 className='text-lg font-semibold'>Order Summary</h4>
-                <p>Total Price: ${price}</p>
-                <p>Number of Items: ${cart.length}</p>
+                <p>Total Price: Rs. {price}</p>
+                <p>Number of Items: {cart.length}</p>
             </div>
 
             <div className='md:w-1/3 space-y-6 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl px-4 py-8'>
